13fec81163c2258bb980c5d5305480b9eb11c64b
om-404 <149902442+om-404@users.noreply.github.com>
2023-12-19 23:33:57 +0530

    02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,6 +1,6 @@
-"use strict"; // treat all JS code as newer version
+"use strict"; //it will treat all JS code as newer version
 
-// alert( 3 + 3) // we are using nodejs, not browser
+// alert( 3 + 3) // this generate output in console but we are using nodejs, not browser
 
 console.log(3 
     +
@@ -8,19 +8,28 @@ console.log(3
 
 console.log("Hitesh")
 
+// sources of JavaScript on internet - tc39.es
+//                                   - mdn
 
 let name = "hitesh"
 let age = 18
 let isLoggedIn = false
 let state;
 
-// number => 2 to power 53
-// bigint
-// string => ""
+// number => range - 2 to power 53
+// bigint - for large numbers
+// string => "  "
 // boolean => true/false
-// null => standalone value
-// undefined => 
+// null => standalone value - representation of empty value which we can assigned to the variable
+// undefined =>  variable is defined but the value is not assigned
 // symbol => unique
+
+// object
+
+console.log(typeof "om")
+console.log(typeof age)
+console.log(typeof null) ----- it is a object
+console.log(typeof undefined)
 
 
 // object
