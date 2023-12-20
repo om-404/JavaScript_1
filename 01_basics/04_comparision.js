a204ab64f7c9ecb1f56c53379f9b2a5c6af0fb68
om-404 <149902442+om-404@users.noreply.github.com>
2023-12-20 15:01:12 +0530

    04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,17 +5,23 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1);   // Js automatic convert string to number but this is bad practice.
+// console.log("02" > 1);  // always insure that you compare only same datatypes.
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0);  // output - false
+console.log(null == 0); // output - false 
+console.log(null >= 0);  // output - true  // note - the reason is that equality and comparison
+                         // work differently , comparison convert null to the number, treating
+                         // it 0 thats why null >= 0 is true
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0);   // output -  false 
+console.log(undefined > 0);    // output -  false
+console.log(undefined < 0);    // output -  true
 
-// === 
+// == double equal, === triple equal work diffrently
+
+// ===   it check value as well as datatypes
 
 console.log("2" === 2);
+
+///////////////////  AVOID SUCH COMPARISON IN CODE //////////////////
